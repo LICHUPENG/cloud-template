6f836251706258aeba42026c93aab152d48191c1
Lichupeng <lichupeng@outlook.com>
2019-11-11 23:52:34 +0800

    更新部分错误

--- template-config/src/main/java/cn/lcp/config/ConfigApplication.java
+++ template-config/src/main/java/cn/lcp/config/ConfigApplication.java
@@ -1,18 +1,16 @@
-package cn.lcp.admin;
+package cn.lcp.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/**
- * 服务提供方 - admin模块
- *
- */
+@EnableConfigServer
 @EnableEurekaClient
 @SpringBootApplication
-public class AdminApplication {
+public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 }
